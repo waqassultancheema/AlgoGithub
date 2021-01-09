7e4e47537294b3f6bf05e291b2050c97da2ba06d
Waqas Sultan <13622096+waqassultancheema@users.noreply.github.com>
2021-01-09 22:15:21 +0400

    Update Node.swift

--- Algo/Algo/Node.swift
+++ Algo/Algo/Node.swift
@@ -8,6 +8,15 @@
 
 import UIKit
 
+
+extension Array {
+    func chunked(into size: Int) -> [[Element]] {
+        return stride(from: 0, to: count, by: size).map {
+            Array(self[$0 ..< Swift.min($0 + size, count)])
+        }
+    }
+}
+
 extension Array {
     
     
